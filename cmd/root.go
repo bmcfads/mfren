75957c5ea0e7faba6417a004205006394d37c857
Brian McFadden <101163132+bmcfads@users.noreply.github.com>
2026-03-23 12:49:11 -0700

    Add directory argument parsing and validation

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,42 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/bmcfads/mfren/internal/renamer"
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:     "mfren <directory>",
 	Short:   "Rename media files after shoot",
 	Version: "0.1.0",
+	Args:    cobra.ExactArgs(1),
+	RunE:    run,
 }
 
 func Execute() error {
+	rootCmd.SilenceErrors = true
+	rootCmd.SilenceUsage = true
 	return rootCmd.Execute()
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	dir, err := filepath.Abs(args[0])
+	if err != nil {
+		return fmt.Errorf("cannot resolve directory path: %w", err)
+	}
+
+	info, err := os.Stat(dir)
+	if err != nil {
+		return fmt.Errorf("cannot access directory: %w", err)
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("%s is not a directory", dir)
+	}
+
+	return renamer.Rename(dir)
 }
--- internal/renamer/renamer.go
+++ internal/renamer/renamer.go
@@ -1,1 +1,8 @@
 package renamer
+
+import "fmt"
+
+func Rename(dir string) error {
+	fmt.Println("Directory:", dir)
+	return nil
+}
